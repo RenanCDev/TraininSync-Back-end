27bd4645aa617f237a54fdcccaf07bc41e976800
raelaraujo0 <raellyedson1@gmail.com>
2025-04-17 10:35:01 -0300

    feat: add crud Aluno model and serializer #51

--- traininSync-back/traininsync/serializers.py
+++ traininSync-back/traininsync/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 
-from .models import Personal, DadosBancarios
+from .models import Personal, DadosBancarios, Pessoa, Aluno
 
 
 class DadosBancariosSerializer(serializers.ModelSerializer):
     class Meta:
         model = DadosBancarios
+        fields = '__all__'
+
+class PessoaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pessoa
         fields = '__all__'
 
 class PersonalSerializer(serializers.ModelSerializer):
@@ -28,6 +33,32 @@ class PersonalSerializer(serializers.ModelSerializer):
             for attr, value in dados_bancarios_data.items():
                 setattr(instance.dados_bancarios, attr, value)
             instance.dados_bancarios.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class AlunoSerializer(serializers.ModelSerializer):
+    pessoa = PessoaSerializer()
+
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
+    def create(self, validated_data):
+        pessoa_data = validated_data.pop('pessoa')
+        pessoa = Pessoa.objects.create(**pessoa_data)
+        aluno = Aluno.objects.create(pessoa=pessoa, **validated_data)
+        return aluno
+
+    def update(self, instance, validated_data):
+        pessoa_data = validated_data.pop('pessoa', None)
+
+        if pessoa_data:
+            for attr, value in pessoa_data.items():
+                setattr(instance.pessoa, attr, value)
+            instance.pessoa.save()
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
