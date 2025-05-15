dd7ae4ba9e2ae142c7e33c5eff565905d999b942
raelaraujo0 <raellyedson1@gmail.com>
2025-05-15 07:38:59 -0300

    feat: creatin test file for pagamento #93

--- traininSync-back/traininsync/tests/models_pagamento_test.py
+++ traininSync-back/traininsync/tests/models_pagamento_test.py
@@ -1,87 +1,82 @@
 from django.test import TestCase
-from datetime import date, time
-from ..models import (Pessoa, Personal, DadosBancarios, 
-                     Aluno, Servico, ContratoDeServico, Pagamento)
+from datetime import date
+
+from ..models import Pessoa, Aluno, Personal, DadosBancarios, Servico, ContratoDeServico, Pagamento
 
 class PagamentoTestCase(TestCase):
     def setUp(self):
-        # Create Pessoa for Personal
-        pessoa_personal = Pessoa.objects.create(
-            nome='Personal Trainer',
-            cpf='55566677788',
-            data_de_nascimento='1990-01-01',
-            email='personal@example.com',
-            numero_de_celular='11987654321',
-            sexo='F',
-            etnia='amarela',
-            estado_civil='solteiro'
+        pessoa = Pessoa.objects.create(
+            nome="Aluno Teste",
+            cpf="12345678909",
+            data_de_nascimento="1995-01-01",
+            email="aluno@teste.com",
+            numero_de_celular="11999999999",
+            sexo="M",
+            etnia="branca",
+            estado_civil="solteiro",
         )
-        
-        # Create Pessoa for Aluno
-        pessoa_aluno = Pessoa.objects.create(
-            nome='Aluno Pagamento',
-            cpf='66677788899',
-            data_de_nascimento='2000-01-01',
-            email='aluno@example.com',
-            numero_de_celular='11987651234',
-            sexo='M',
-            etnia='branca',
-            estado_civil='solteiro'
-        )
-        
-        # Create DadosBancarios
-        dados_bancarios = DadosBancarios.objects.create(
-            numero_conta='654321',
-            agencia='4321'
-        )
-        
-        # Create Personal
-        self.personal = Personal.objects.create(
-            pessoa_ptr=pessoa_personal,
-            cref='CREF456',
-            especialidades='Pilates',
-            experiencia_profissional='3 anos',
-            dados_bancarios=dados_bancarios,
-            horarios_disponiveis=8.0,
-            locais_disponiveis='Studio Fitness'
-        )
-        
-        # Create Aluno
         self.aluno = Aluno.objects.create(
-            pessoa=pessoa_aluno,
-            bioimpedancia='BIO456',
-            altura=1.65,
-            peso=60.0,
-            idade=22,
-            sexo='F',
-            data_do_Exame='2023-01-01',
-            hora_do_Exame='14:00:00'
+            pessoa=pessoa,
+            bioimpedancia="BIO1234567890",
+            altura=1.75,
+            data_do_exame=date.today(),
+            hora_do_exame="08:00",
+            peso=70.0,
         )
-        
-        # Create Servico
+        dados_bancarios = DadosBancarios.objects.create(numero_conta="123456", agencia="0001")
+        personal = Personal.objects.create(
+            nome="Personal Teste",
+            cpf="98765432100",
+            data_de_nascimento="1980-01-01",
+            email="personal@teste.com",
+            numero_de_celular="11988888888",
+            sexo="M",
+            etnia="branca",
+            estado_civil="solteiro",
+            cref="CREF54321",
+            especialidades="Musculação",
+            experiencia_profissional="10 anos",
+            dados_bancarios=dados_bancarios,
+            horarios_disponiveis=20.0,
+            locais_disponiveis="Academia Central",
+        )
         servico = Servico.objects.create(
-            tipo_de_servico='Pilates',
-            descricao_do_servico='Aulas de Pilates',
-            valr_do_servico=150.0
+            tipo_de_servico="Treino Avançado",
+            descricao_do_servico="Treino focado em hipertrofia",
+            valor_do_servico=200.0
         )
-        
-        # Create Contrato
         self.contrato = ContratoDeServico.objects.create(
-            personal=self.personal,
+            personal=personal,
             aluno=self.aluno,
             servico_desejado=servico,
-            localidade_desejada='Studio Fitness'
+            localidade_desejada="Academia Central"
         )
-        
-        # Create Pagamento
         self.pagamento = Pagamento.objects.create(
             aluno=self.aluno,
             contrato=self.contrato,
-            valor=500.00,
-            descricao='Mensalidade Janeiro/2023'
+            valor=200.00,
+            descricao="Pagamento mensal do serviço"
         )
 
-    def test_create_pagamento(self):
-        self.assertEqual(float(self.pagamento.valor), 500.00)
-        self.assertEqual(self.pagamento.aluno.pessoa.nome, 'Aluno Pagamento')
-        self.assertEqual(self.pagamento.contrato.servico_desejado.tipo_de_servico, 'Pilates')
+    def test_criar_pagamento(self):
+        self.assertEqual(self.pagamento.aluno, self.aluno)
+        self.assertEqual(self.pagamento.contrato, self.contrato)
+        self.assertEqual(self.pagamento.valor, 200.00)
+        self.assertIn("Pagamento mensal", self.pagamento.descricao)
+
+    def test_consultar_pagamento(self):
+        pagamento_consultado = Pagamento.objects.filter(descricao__icontains="mensal").first()
+        self.assertIsNotNone(pagamento_consultado)
+        self.assertEqual(pagamento_consultado.id, self.pagamento.id)
+
+    def test_ativar_pagamento(self):
+        self.pagamento.descricao = "Pagamento Ativo"
+        self.pagamento.save()
+        pagamento_db = Pagamento.objects.get(id=self.pagamento.id)
+        self.assertEqual(pagamento_db.descricao, "Pagamento Ativo")
+
+    def test_desativar_pagamento(self):
+        self.pagamento.descricao = "Pagamento Desativado"
+        self.pagamento.save()
+        pagamento_db = Pagamento.objects.get(id=self.pagamento.id)
+        self.assertEqual(pagamento_db.descricao, "Pagamento Desativado")
