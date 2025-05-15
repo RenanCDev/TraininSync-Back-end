37561e5f3122ecfeccda9de5b565bee6b763975d
raelaraujo0 <raellyedson1@gmail.com>
2025-05-15 07:38:27 -0300

    feat: creatin test file for dados bancarios#93

--- traininSync-back/traininsync/tests/models_dados_bancarios_test.py
+++ traininSync-back/traininsync/tests/models_dados_bancarios_test.py
@@ -2,7 +2,29 @@ from django.test import TestCase
 from ..models import DadosBancarios
 
 class DadosBancariosTestCase(TestCase):
-    def test_create_dados_bancarios(self):
-        dados = DadosBancarios.objects.create(numero_conta='123456', agencia='1234')
-        self.assertEqual(dados.numero_conta, '123456')
-        self.assertEqual(dados.agencia, '1234')
+    def setUp(self):
+        self.dados_bancarios = DadosBancarios.objects.create(
+            numero_conta="123456",
+            agencia="0001",
+        )
+
+    def test_criar_dados_bancarios(self):
+        self.assertEqual(self.dados_bancarios.numero_conta, "123456")
+        self.assertEqual(self.dados_bancarios.agencia, "0001")
+
+    def test_desativar_dados_bancarios(self):
+        self.dados_bancarios.ativo = False
+        self.dados_bancarios.save()
+        dados_db = DadosBancarios.objects.get(id=self.dados_bancarios.id)
+
+    def test_ativar_dados_bancarios(self):
+        self.dados_bancarios.ativo = False
+        self.dados_bancarios.save()
+        self.dados_bancarios.ativo = True
+        self.dados_bancarios.save()
+        dados_db = DadosBancarios.objects.get(id=self.dados_bancarios.id)
+
+    def test_consultar_dados_bancarios(self):
+        dados_consultado = DadosBancarios.objects.filter(numero_conta="123456").first()
+        self.assertIsNotNone(dados_consultado)
+        self.assertEqual(dados_consultado.id, self.dados_bancarios.id)
